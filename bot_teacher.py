a00df8799bad2cd4ef94609b810294daeab616ce
Alexander <MeisterUrian@mail.ru>
2017-07-12 23:03:20 +0900

    break code into functions with loops until current intent released

--- bot_teacher.py
+++ bot_teacher.py
@@ -23,74 +23,103 @@ def recognize_speech():
     speech_text = speech_recognizer.recognize_speech(audio_format_converter.raw_audio2wav(speech_audio, pyaudio_config))
     return speech_text
 
-def main():
+#bad speech recognition client phrase
+BAD_SPEECH_RECOGNITION = 'Повторите, пожалуйста, еще раз. Плохо слышно'
 
+#----------------------------------------------------------------
+log = None
+
+def welcome():
+    global log
+    print('listen...\n')
+    speech_text = recognize_speech()
+    bot_answer = bot.request(speech_text)
+    if bot_answer['intent_name'] == '1.Welcome':
+        print('Consultant > ', speech_text)
+        print('Client > ', bot_answer['text'])
+
+        log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
+
+        if bot_answer['text'] == 'Добрый день' or bot_answer['text'] == 'Доброе утро' or bot_answer['text'] == 'Добрый вечер' or bot_answer['text'] == 'Здравствуйте':
+            approach()
+        elif bot_answer['text'] == 'Здравствуйте! Я ищу утюг, который бы автоматически отключался, если его не выключили':
+            needs_detection()
+    else:
+        print(BAD_SPEECH_RECOGNITION)
+        welcome()
+
+
+def approach():
+    global log
+    #listen to consultor and notify the time
+    speech_text = str()
+    start = time.time()
+    while time.time() - start < 90:
+        print('listen...\n')
+        speech_text = recognize_speech()
+        if speech_text is not None and len(speech_text) > 0:
+            break
+    
+    pause = time.time() - start
+
+    if pause < 30:
+        #approach-decision-leave
+        bot_answer = bot.request('goodbye')
+        print('Consultant > ', speech_text)
+        print('Client > ', bot_answer['text'])
+        log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
+    elif pause < 90:
+        #approach
+        bot_answer = bot.request(speech_text)
+        if bot_answer['intent_name'] == '2-Approach':
+            print('Consultant > ', speech_text)
+            #approach-decision-continue
+            bot_answer = bot.request('continue')
+            print('Client > ', bot_answer['text'])
+            log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
+            #product presentation
+            #NEED FOR PRODUCT PRESENTATION SCENARIO
+
+        else:
+            print(BAD_SPEECH_RECOGNITION)
+            approach()
+
+    else:
+        print('Client gone!')
+
+
+def needs_detection():
+    global log
+    print('listen...\n')
+    speech_text = recognize_speech()
+    bot_answer = bot.request(speech_text)
+    if bot_answer['intent_name'] == '3-Needs-detection':
+        print('Consultant > ', speech_text)
+        print('Client > ', bot_answer['text'])
+        log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
+        #product presentation
+        #NEED FOR PRODUCT PRESENTATION SCENARIO
+
+    else:
+        print(BAD_SPEECH_RECOGNITION)
+        needs_detection()
+        
+#----------------------------------------------------------------
+
+def main():
+    global log    
     log = open('stat/' + time.ctime() + '.log', 'w')
+    
     print('listen...\n')
     #welcome
     try:
-        speech_text = recognize_speech()
-        bot_answer = bot.request(speech_text)
-        if bot_answer['intent_name'] == '1.Welcome':
-            print('Consultant > ', speech_text)
-            print('Client > ', bot_answer['text'])
-
-            log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-
-            if bot_answer['text'] == 'Добрый день' or bot_answer['text'] == 'Доброе утро' or bot_answer['text'] == 'Добрый вечер' or bot_answer['text'] == 'Здравствуйте':
-
-                #listen to consultor and notify the time
-                speech_text = str()
-                start = time.time()
-                while time.time() - start < 90:
-                    speech_text = recognize_speech()
-                    if speech_text is not None and len(speech_text) > 0:
-                        break
-                
-                pause = time.time() - start
-
-                if pause < 30:
-                    #approach-decision-leave
-                    bot_answer = bot.request('goodbye')
-                    print('Consultant > ', speech_text)
-                    print('Client > ', bot_answer['text'])
-                    log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-                elif pause < 90:
-                    #approach
-                    bot_answer = bot.request(speech_text)
-                    if bot_answer['intent_name'] == '2-Approach':
-                        print('Consultant > ', speech_text)
-                        #approach-decision-continue
-                        bot_answer = bot.request('continue')
-                        print('Client > ', bot_answer['text'])
-                        log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-                        #product presentation
-
-                else:
-                    print('Client gone!')
-
-
-            elif bot_answer['text'] == 'Здравствуйте! Я ищу утюг, который бы автоматически отключался, если его не выключили':
-                print('Consultant > ', speech_text)
-                print('Client > ', bot_answer['text'])
-                log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-                #needs-detection
-                print('listen...\n')
-                speech_text = recognize_speech()
-                bot_answer = bot.request(speech_text)
-                if bot_answer['intent_name'] == '3-Needs-detection':
-                    print('Consultant > ', speech_text)
-                    print('Client > ', bot_answer['text'])
-                    log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-                    #product presentation
-                    #Consultant must make product presentation
-
-        log.close()
-        print('Repeat?')
-        speech_text = recognize_speech()
-        if speech_text is not None and speech_text.strip().startswith('да'):
-            main()
-
+       welcome()
+       log.close()
+       print('Repeat?')
+       speech_text = recognize_speech()
+       if speech_text is not None and speech_text.strip().startswith('да'):
+           main()
+           
     except Exception as e:
         ErrorLogger(__file__, e)
 
