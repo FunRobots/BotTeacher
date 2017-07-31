c1012a120db4c210a4b4e6b84c89d17e64dfa0b4
Alexander <MeisterUrian@mail.ru>
2017-07-31 21:35:19 +0900

    release scenario schemes

--- scripts/bot_teacher.py
+++ scripts/bot_teacher.py
@@ -11,7 +11,7 @@ import json
 import time
 from bt_audio.utils import audio_format_converter
 import os
-
+from pathlib import Path
 from bt_utils import ErrorLogger
 
 #read config
@@ -52,113 +52,6 @@ STAT_FOLDER = 'stat'
 #----------------------------------------------------------------
 log = None
 
-def welcome():
-    global log
-    global WELCOME_STATUS
-
-    print('listen...\n')
-    speech_text = recognize_speech()
-    bot_answer = bot.request(speech_text)
-    if isinstance(bot_answer, dict) and bot_answer.get('intent_name') == '1.Welcome':
-        print('Consultant > ', speech_text)
-        print('Client > ', bot_answer['text'])
-        say_text(bot_answer['text'])
-
-        log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-
-##        if bot_answer['text'] == 'Добрый день' or bot_answer['text'] == 'Доброе утро' or bot_answer['text'] == 'Добрый вечер' or bot_answer['text'] == 'Здравствуйте':
-##            approach()
-##        elif bot_answer['text'] == 'Здравствуйте! Я ищу утюг, который бы автоматически отключался, если его не выключили':
-##            needs_detection()
-        WELCOME_STATUS = True
-        return True
-
-    else:
-        print(BAD_SPEECH_RECOGNITION)
-        say_text(BAD_SPEECH_RECOGNITION)
-##        welcome()
-        WELCOME_STATUS = False
-        return False
-
-def approach():
-    global log
-    global APPROACH_STATUS
-
-    #listen to consultor and notify the time
-    speech_text = str()
-    start = time.time()
-    while time.time() - start < 90:
-        print('listen...\n')
-        speech_text = recognize_speech()
-        if speech_text is not None and len(speech_text) > 0:
-            break
-
-    pause = time.time() - start
-
-    if pause < 30:
-        #approach-decision-leave
-        bot_answer = bot.request('goodbye')
-        print('Consultant > ', speech_text)
-        print('Client > ', bot_answer.get('text') if isinstance(bot_answer, dict) else str() )
-        say_text(bot_answer['text'])
-        log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-        APPROACH_STATUS = None
-        return None
-
-    elif pause < 90:
-        #approach
-        bot_answer = bot.request(speech_text)
-        if isinstance(bot_answer, dict) and bot_answer.get('intent_name') == '2-Approach':
-            print('Consultant > ', speech_text)
-            #approach-decision-continue
-            bot_answer = bot.request('continue')
-            print('Client > ', bot_answer.get('text') if isinstance(bot_answer, dict) else str() )
-            say_text(bot_answer['text'])
-            log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-            #product presentation
-            #NEED FOR PRODUCT PRESENTATION SCENARIO
-            APPROACH_STATUS = True
-            return True
-        else:
-            print(BAD_SPEECH_RECOGNITION)
-            say_text(BAD_SPEECH_RECOGNITION)
-##            approach()
-            APPROACH_STATUS = False
-            return False
-
-    else:
-        print('Client gone!')
-        say_text('Клиент ушел!')
-        APPROACH_STATUS = None
-        return None
-
-
-def needs_detection():
-    global log
-    global NEEDS_DETECTION_STATUS
-
-    print('listen...\n')
-    speech_text = recognize_speech()
-    bot_answer = bot.request(speech_text)
-    if isinstance(bot_answer, dict) and bot_answer.get('intent_name') == '3-Needs-detection':
-        print('Consultant > ', speech_text)
-        print('Client > ', bot_answer.get('text') if isinstance(bot_answer, dict) else str() )
-        say_text(bot_answer['text'])
-        log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
-        #product presentation
-        #NEED FOR PRODUCT PRESENTATION SCENARIO
-
-        NEEDS_DETECTION_STATUS = True
-        return True
-
-    else:
-        print(BAD_SPEECH_RECOGNITION)
-        say_text(BAD_SPEECH_RECOGNITION)
-##        needs_detection()
-        NEEDS_DETECTION_STATUS = False
-        return False
-#----------------------------------------------------------------
-
 def main():
     global log
     global STAT_FOLDER
@@ -168,17 +61,52 @@ def main():
     log = open('stat/' + time.ctime() + '.log', 'w')
 
     try:
-        while WELCOME_STATUS is  False:
-            welcome()
+        scheme_file = Path(__file__).resolve().parents[1].as_posix() + '/run/' + rospy.get_param('scheme_file')
+        scheme = json.load(open(scheme_file,'r'))
 
-        while APPROACH_STATUS is False:
-            approach()
+        current_intent = scheme.get('start')
+        while current_intent is not None:
+            speech_text = str()
+            start = time.time()
+            while time.time() - start < 90:
+                print('listen...\n')
+                speech_text = recognize_speech()
+                if speech_text is not None and len(speech_text) > 0:
+                    break
 
-        if APPROACH_STATUS is True:
-            while NEEDS_DETECTION_STATUS is False:
-                needs_detection()
-        elif APPROACH_STATUS is None:
-            pass
+            pause = time.time() - start
+            print('Consultant > ', speech_text)
+
+            wait_cases_dict = scheme['intents'][current_intent].get('wait_before_transition')
+            if wait_cases_dict is not None:
+                wait_keys = [int(i) for i in wait_cases_dict.keys()].sort()
+                case_key_index = 1
+                while case_key_index < len(wait_keys) and pause < wait_keys[case_key_index + 1]:
+                    case_key += 1
+
+                case = wait_cases_dict[str(wait_keys[case_key_index])]
+
+                phrase_to_say = case.get('say_phrase')
+                if phrase_to_say is not None:
+                    bot_answer = bot.request(phrase_to_say)
+                    print('Client > ', bot_answer.get('text'))
+                    say_text(bot_answer.get('text'))
+                    log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
+                else:
+                    bot_answer = bot.request(speech_text)
+                    if isinstance(bot_answer, dict) and bot_answer.get('intent_name') == current_intent:
+                        bot_answer = bot.request('continue')
+                        if isinstance(bot_answer, dict):
+                            print('Client > ', bot_answer.get('text'))
+                            say_text(bot_answer.get('text'))
+                            log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), bot_answer['intent_name'], speech_text, bot_answer['text']))
+
+
+                current_intent = case.get('goto_next')
+
+            else:
+                current_intent = scheme['intents'][current_intent].get('goto_next')
+
 
         log.close()
         print('Repeat?')
@@ -188,6 +116,7 @@ def main():
 
     except Exception as e:
         ErrorLogger(__file__, e)
+
 
 if __name__ == '__main__':
     rospy.init_node('bot_teacher')
