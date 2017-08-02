da223aea720b586dfdd740434f263e59cc5b758b
Alexander <MeisterUrian@mail.ru>
2017-08-02 18:37:46 +0900

    add comments

--- scripts/bot_teacher.py
+++ scripts/bot_teacher.py
@@ -17,8 +17,14 @@ from typing import List
 
 
 class SchemeProcessor:
+    '''
+    Allows to process BotTeacher scenario scheme
+    '''
 
     def __init__(self, yandex_voice_key, apiai_bot_client_key, pyaudio_config, scheme_file):
+        '''
+        Constructor
+        '''
 
         #bad speech recognition client phrase
         self.BAD_SPEECH_RECOGNITION = 'Повторите, пожалуйста, еще раз. Плохо слышно'
@@ -45,7 +51,6 @@ class SchemeProcessor:
 
         self.bot_answer_text = str()
         self.pause = 0
-        self.do_bot_request_by_consultant_speech = True
         #self.current_intent_changed = False
 
     def _recognize_speech(self) -> str:
@@ -69,11 +74,19 @@ class SchemeProcessor:
         return json.load(open(scheme_file,'r'))
 
     def _goto_next(self, intent_name: str) -> None:
+        '''
+        set next intent name
+        '''
+        
         self.current_intent_name = intent_name
         #self.current_intent_changed = True
         return None
 
-    def _say_phrase(self, phrase_to_say: str) -> List[str] or None:
+    def _say_phrase(self, phrase_to_say: str) -> str or None:
+        '''
+        say phrase to the bot and return text answer or None
+        '''
+        
         self.do_bot_request_by_consultant_speech = False
 
         bot_answer = self.bot.request(phrase_to_say)
@@ -82,25 +95,23 @@ class SchemeProcessor:
         else:
             return None
 
-    def _wait_before_transition(self, variants_dict: dict) -> List[str] or None:
-        self.do_bot_request_by_consultant_speech = False
+    def _wait_before_transition(self, variants_dict: dict) -> str or None:
+        '''
+        switch to one of variants depending of pause duration
+        '''
 
-        wait_keys = [int(i) for i in variants_dict.keys()]
-        wait_keys.sort()
-
-        print('wait_keys', wait_keys)
-        print('pause', self.pause)
-
-        case_key_index = 0
+        wait_keys = [int(i) for i in variants_dict.keys()] #all pause duration variants (keys of variants dictionary)
+        wait_keys.sort() #sort variants
+        
+        case_key_index = 0 #find variant index
         while case_key_index < len(wait_keys) and self.pause > wait_keys[case_key_index]:
-            print('|')
             case_key_index += 1
 
-        case = variants_dict[str(wait_keys[case_key_index])]
-        print('case', case)
-        self.current_intent_name = case.get('goto_next')
-
-        phrase_to_say = case.get('say_phrase')
+        case = variants_dict[str(wait_keys[case_key_index])] #choose case (action variant) in according to pause)
+        
+        self.current_intent_name = case.get('goto_next') #read next intent name
+        
+        phrase_to_say = case.get('say_phrase') #read phrase to send to bot
         if phrase_to_say is not None:
             bot_answer = self.bot.request(phrase_to_say)
             if isinstance(bot_answer, dict):
@@ -108,10 +119,14 @@ class SchemeProcessor:
             else:
                 return None
 
-        return case.get('return')
+        return case.get('return') #if case has 'return' key return it value
 
     def process(self):
+        '''
+        process all scheme
+        '''
 
+        #prepare statistics collection
         self.STAT_FOLDER = 'stat'
         if os.path.exists(self.STAT_FOLDER) is False:
             os.mkdir(self.STAT_FOLDER)
@@ -130,28 +145,31 @@ class SchemeProcessor:
 
             self.pause = time.time() - start
             print('Consultant > ', speech_text)
+
+            #request to bot Consultant speech content
             bot_answer = self.bot.request(speech_text)
-            if isinstance(bot_answer, dict) and bot_answer.get('intent_name') == self.current_intent_name:
-                current_intent = self.scheme['intents'].get(self.current_intent_name)
+            if isinstance(bot_answer, dict) and bot_answer.get('intent_name') == self.current_intent_name: #if intent name in bot answer is equal to current intent name
+                current_intent = self.scheme['intents'].get(self.current_intent_name) #try extract intent dictionary
                 if isinstance(current_intent, dict):
-                    variants_dict = current_intent.get('wait_before_transition')
+                    variants_dict = current_intent.get('wait_before_transition') #extract pause-depending variants 
                 else:
                     variants_dict = None
                     current_intent = dict()
 
                 if variants_dict is not None:
+                    #proccess pause-depending variants dictionary
                     self.log.write('\n\n [{0}] \n Intent: {1} \n Consultant > {2}'.format(time.ctime(), self.current_intent_name, speech_text))
                     bot_answer_text = self._wait_before_transition(variants_dict=variants_dict)
                     print('Client > ', bot_answer_text)
                     self._say_text(bot_answer_text)
                     self.log.write('\n Client > {0}'.format(bot_answer_text))
-                else:
+                else: #if current intent has no field 'wait_before_transition' just communicate with bot
                     print('Client > ', bot_answer.get('text'))
                     self._say_text(bot_answer.get('text'))
                     self.log.write('[{0}] \n Intent: {1} \n Consultant > {2} \n Client > {3}'.format(time.ctime(), self.current_intent_name, speech_text, bot_answer['text']))
                     self.current_intent_name = current_intent.get('goto_next')
 
-            else:
+            else: #if intent name in bot answer is not equal to current intent name inform about it
                 print(self.BAD_SPEECH_RECOGNITION)
                 self._say_text(self.BAD_SPEECH_RECOGNITION)
                 continue
